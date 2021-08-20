64643dc6bf8f2cfd69009848f009ca873ccf77b3
saadkhan955 <saad.khan955@gmail.com>
2021-08-20 18:35:49 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -56,6 +56,16 @@ function watchTask() {
 	);
 }
 // webserv config
-
+gulp.task('publish', function (cb) {
+	buildBranch({
+		branch: 'build',
+		ignore: ['.git', '.token', 'www', 'node_modules']
+	}, function (err) {
+		if (err) {
+			throw err;
+		}
+		cb();
+	});
+});
 // Default Gulp Task
 exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
