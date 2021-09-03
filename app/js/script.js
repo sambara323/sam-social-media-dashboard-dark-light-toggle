23f873327b9b3811689e1e3f6e60288044858c13
sambara323 <71379562+sambara323@users.noreply.github.com>
2021-09-03 17:52:43 +0530

    Third Commit

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,55 @@
-console.log('HELLO WORLD!');
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
+
+const setTheme = () => {
+  if (localStorage.getItem("theme") == "dark") {
+    setDarkTheme();
+    darkButton.click();
+  } else if (localStorage.getItem("theme") == "light") {
+    setLightTheme();
+    lightButton.click();
+  }
+};
+
+const checkMode = () => {
+  if (localStorage.getItem("theme") == null) {
+    if (windows.matchMedia("(prefers-color-scheme: light)").match) {
+      lightButton.click();
+    } else if (windows.matchMedia("(prefers-color-scheme: dark)").match) {
+      darkButton.click();
+    }
+  }
+};
+
+const checkModeChange = () => {
+  windows
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (e) => {
+      checkMode();
+    });
+};
+
+const setDarkTheme = () => {
+  document.querySelector("body").classList = "dark";
+};
+
+const setLightTheme = () => {
+  document.querySelector("body").classList = "light";
+};
+
+setTheme();
+checkMode();
+checkModeChange();
+
+const radioButton = document.querySelectorAll(".toggle__wrapper input");
+for (let i = 0; i < radioButton.length; i++) {
+  radioButton[i].addEventListener("click", (event) => {
+    if (darkButton.checked) {
+      localStorage.setItem("theme", "dark");
+      setDarkTheme();
+    } else {
+      localStorage.setItem("theme", "light");
+      setLightTheme();
+    }
+  });
+}
